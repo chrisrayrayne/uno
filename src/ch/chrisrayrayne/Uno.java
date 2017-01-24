9a11d9a66f967beb30d092fe3a54fb27dd532266
chrisrayrayne <chris.ray.rayne@gmail.com>
2017-01-24 17:28:01 +0100

    Add files via upload

---
+++ src/ch/chrisrayrayne/Uno.java
@@ -0,0 +1,14 @@
+package ch.chrisrayrayne;
+
+/**
+ * Created by chrisrayrayne on 30.12.16.
+ */
+public class Uno {
+
+    public static void main(String[] args) {
+        Game g = new Game();
+
+        g.init();
+        g.play();
+    }
+}
